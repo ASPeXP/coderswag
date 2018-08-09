9088bf893265b1b0e17e5e590eb6be6d5208f693
Prapath Suayroop <prapaths@hotmail.com>
2018-08-09 09:15:09 +0700

    lesson 2

--- app/src/main/java/com/ipathsaid/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/ipathsaid/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,30 @@ package com.ipathsaid.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.ipathsaid.coderswag.Adapters.CategoryAdapter
+import com.ipathsaid.coderswag.Model.Category
 import com.ipathsaid.coderswag.R
+import com.ipathsaid.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      //  adapter = ArrayAdapter(this,
+       //         android.R.layout.simple_list_item_1,
+         //       DataService.categories)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+
+        categoryListView.adapter = adapter
+
+
+
     }
 }
--- app/src/main/java/com/ipathsaid/coderswag/Model/Category.kt
+++ app/src/main/java/com/ipathsaid/coderswag/Model/Category.kt
@@ -1,5 +1,7 @@
 package com.ipathsaid.coderswag.Model
 
 class Category(val title: String, val image: String ){
-
+    override fun toString(): String {
+        return title
+    }
 }
